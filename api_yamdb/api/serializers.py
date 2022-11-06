85e069e4effedb6db66927c5f22f55902bac42d5
Александр Охновский <alexander-okhnovsky@mail.ru>
2022-11-06 22:31:49 +0300

    Fix bugs

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -106,35 +106,33 @@ class ReadOnlyTitleSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    title = serializers.SlugRelatedField(
-        slug_field='name',
-        read_only=True
-    )
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
     )
 
     def validate_score(self, value):
-        if 0 > value > 10:
-            raise serializers.ValidationError('Оценка по 10-бальной шкале!')
-        return value
+        if 0 < value < 11:
+            return value
+        raise serializers.ValidationError(
+            'Оценка от 1 до 10'
+        )
 
-    def validate(self, data):
+    def validate(self, value):
         request = self.context['request']
         author = request.user
         title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        if (
-            request.method == 'POST'
-            and Review.objects.filter(title=title, author=author).exists()
-        ):
-            raise ValidationError('Может существовать только один отзыв!')
-        return data
+        if (request.method == 'POST' and Review.objects.filter(
+            title=title, author=author
+        ).exists()):
+            raise ValidationError('Вы можете добавить только один отзыв')
+        return value
 
     class Meta:
-        fields = '__all__'
         model = Review
+        fields = ('__all__')
+        read_only_fields = ('title', 'author')
 
 
 class CommentSerializer(serializers.ModelSerializer):
